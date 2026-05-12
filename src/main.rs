e86d2d0e7cfe3a7e35e53740839d46fb988e4ce9
Omar Elbaghdadi <omar@attendi.nl>
2026-05-12 22:14:04 +0200

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 /// A "Hello, world!" program.
 
 fn main() {
-    print_hello();
-    print_goodbye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_goodbye() {
-    println!("Goodbye, world!");
+// a function that prints a message
+fn print(m: &str) {
+    println!("{m}")
 }
