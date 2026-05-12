0caec1feb9d7d08f24b8564025d0f568670b0eed
Omar Elbaghdadi <omar@attendi.nl>
2026-05-12 21:41:23 +0200

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 /// A "Hello, world!" program.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
